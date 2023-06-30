04340ab809bffe8792e3b26ac488c332ae5ca822
Armand H <armand.hurault@efrei.net>
2023-06-30 13:07:47 +0200

    Added scroll with mouse + geolocation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let map, infoWindow;
+
 function initMap() {
 	// Update MAP_ID with custom map ID
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -12,7 +14,9 @@ function initMap() {
 		//Removing the UI
 		mapTypeControl: false,
 		fullscreenControl: false,
-		streetViewControl: false,
+		//streetViewControl: false,
+		// Enable scrollwheel zooming
+        scrollwheel: true
 	});
 
 	// Creating the custom markers
@@ -48,4 +52,46 @@ function initMap() {
 			infowindow.open(map, marker);
 		});
 	}
+
+	//Button to center on the current location of the user
+	infoWindow = new google.maps.InfoWindow();
+	const locationButton = document.createElement("button");
+
+	locationButton.textContent = "Voir ma position";
+	locationButton.classList.add("custom-map-control-button");
+	map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+	locationButton.addEventListener("click", () => {
+	  // Try HTML5 geolocation.
+	  if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+		  (position) => {
+			const pos = {
+			  lat: position.coords.latitude,
+			  lng: position.coords.longitude,
+			};
+  
+			infoWindow.setPosition(pos);
+			infoWindow.setContent("Location found.");
+			infoWindow.open(map);
+			map.setCenter(pos);
+		  },
+		  () => {
+			handleLocationError(true, infoWindow, map.getCenter());
+		  }
+		);
+	  } else {
+		// Browser doesn't support Geolocation
+		handleLocationError(false, infoWindow, map.getCenter());
+	  }
+	});
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(
+	  browserHasGeolocation
+		? "Error: The Geolocation service failed."
+		: "Error: Your browser doesn't support geolocation."
+	);
+	infoWindow.open(map);
 }
